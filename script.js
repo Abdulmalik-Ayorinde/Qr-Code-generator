7bcfbc951c5c34af6e836a8be73e19ca51f03f7b
Abdulmalik Ayorinde <55638353+Abdulmalik-Ayorinde@users.noreply.github.com>
2020-12-19 18:27:19 +0100

    Working on getting value from input

--- script.js
+++ script.js
@@ -1,103 +1,58 @@
 const QRCode = require('qrcode')
-const canvas = document.getElementById('canvas')
-const error = document.getElementById('error')
 
 
+const codeImg = document.getElementById('code-img')
+const  name = document.getElementById('name')
+const  email = document.getElementById('email')
+const  twitter = document.getElementById('twitter')
+const   github = document.getElementById('github')
+
+const nameVal = 'name' + email.value
+
+const genBtn = document.getElementById('generate')
+const cancleBtn = document.getElementById('cancle')
+const closeBtn = document.getElementById('close-modal')
  
 const data = [
-    { errorCorrectionLevel: 'H' },
-    
+    { errorCorrectionLevel: 'H' }  
 ]
 
-// const text = [
-//     'ullam nobis ut neque magni eos officia adipisci quam?',
-//     'Saepe doloremque autem non labore natus eum sit officiis ex eligendi rem incidunt',
-//     'officiis ex eligendi rem incidunt, ullam nobis ut neque magni eos officia',
-//     'dolor sit amet consectetur adipisicing elit. Saepe doloremque autem non labor',
-//     'ullam nobis ut neque magni eos officia adipisci quam?',
-//     'Saepe doloremque autem non labore natus eum sit officiis ex eligendi rem incidunt',
-//     'officiis ex eligendi rem incidunt, ullam nobis ut neque magni eos officia',
-//     'dolor sit amet consectetur adipisicing elit. Saepe doloremque autem non labor',
-// ]
-
-// const text = [
-//      'facebook.com',
-//      'twitter.com',
-//      'github.com'
-// ]
-
-// QRCode.toDataURL("text", data, error => {
-//   if (error) {
-//       console.error(error)
-//     } else {
-//      console.log('success!');
-//      console.log(data)
-//     }
-// })
-
-// error.addEventListener("change", (data) => {
-//     console.log(data.value.text)
-// })
-
-
-// var opts = {
-//     errorCorrectionLevel: 'H',
-//     type: 'image/jpeg',
-//     quality: 0.3,
-//     margin: 1,
-//     color: {
-//       dark:"#010599FF",
-//       light:"#FFBF60FF"
-//     }
-//   }
-   
-//   QRCode.toDataURL('text', opts, function (err, url) {
-//     if (err) throw err
-   
-//     var img = document.getElementById('image')
-//     img.src = url
-//   })
+console.log(nameVal)
 
 
 
-const text = [
-    'Full Name: Abdulmalik Ayorinde',
+const val = [
+    `Full Name: ${nameVal}`,
     '        ',
     'Twitter: https://twitter.com/AbdulmalikAyor1',
     '        ',
     'Email: malik.ayo4life@gmail.com',
     '        ',
-    'Github: https://github.com/Abdulmalik-Ayorinde'
-    
+    'Github: https://github.com/Abdulmalik-Ayorinde'  
 ]
 
-// text.push(`${<h1>google.com/ayo</h1>}`)
+val.push(nameVal)
 
-// console.log(text[1])
-
-// const head  = document.createElement('b')
-
-//  head.innerHTML = 'bro'
-
-//  canvas.appendChild(head)
-//  console.log(head)
-
-var segs = [
+const segs = [
     
-    { data: 'Full Name: Abdulmalik Ayorinde', mode: 'Byte' },
-    { data: "   ", mode: 'Byte' },
-    { data: 'Twitter: https://twitter.com/AbdulmalikAyor1', mode: 'Byte' },
-    { data: "   ", mode: 'Byte' },
-    { data: 'Email: malik.ayo4life@gmail.com', mode: 'Byte' },
-    { data: "   ", mode: 'Byte' },
-    { data: 'Github: https://github.com/Abdulmalik-Ayorinde', mode: 'Byte' }
-    // { data: '0123456', mode: 'numeric' }
+    { data: `${val[0]}`},
+    { data: "           ", mode: 'Byte' },
+    { data: `Email: ${email.value}`, mode: 'Byte' },
+    { data: "           ", mode: 'Byte' },
+    { data: `Twitter: ${twitter.value}`, mode: 'Byte' },
+    { data: "           ", mode: 'Byte' },
+    { data: `Github: ${github.value}`, mode: 'Byte' }
   ]
- 
-  QRCode.toDataURL(segs, data, (err, url) => console.log(url))
 
+  console.log(val)
+const generateCode = () => {
+    console.log(val)
+    console.log(email.value)
+    QRCode.toDataURL(val, data, (err, url) => {
+        if(err) console.error(err)
+        console.log(url)
+        codeImg.setAttribute('src', `${url}`)
+      })
+}
 
- 
-//   QRCode.toDataURL(canvas, { toSJISFunc: toSJIS }, function (err, url) {
-//     console.log(url)
-//   })
+genBtn.addEventListener('click', generateCode)
